fc72a7c776a36c01f7d27eaefc5f286c3a439c2e
crawfowa@gmail.com <crawfowa@gmail.com>
2023-09-03 23:15:55 -0700

    Start big refactor, move old source files into seperate directory, for future reference, to delete later.

---
+++ source/EngineScript.py
@@ -0,0 +1,141 @@
+from enum import Enum
+
+class GameState():
+    def __init__(self):
+        # 000 - empty
+        # f* - foot soldier
+        # h* - horse soldier
+        # a* - armored soldier
+        # k* - armored horse soldier (knight)
+        # *s - sword
+        # *l - spear/lance
+        # *b - bow
+        self.map = [
+            [-1, -1, -1, 0, 1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, -1, -1, -1, -1, -1],
+            [-1, -1, -1, 2, 3, -1, -1, -1],
+        ]        
+        self.blueToMove = True
+        self.moveLog = []
+        self.unitList = [FootSoldier("b"), FootSoldier("b"), FootSoldier("r"), FootSoldier("r")]
+
+    def makeMove(self, move):
+        self.map[move.startRow][move.startCol] = -1
+        self.map[move.endRow][move.endCol] = move.pieceMoved
+        self.moveLog.append(move) #log the move for later reference
+        self.blueToMove = not self.blueToMove    
+    
+    def undoMove(self):
+        if len(self.moveLog) != 0:
+            move = self.moveLog.pop()
+            self.map[move.startRow][move.startCol] = move.pieceMoved
+            self.map[move.endRow][move.endCol] = move.pieceCaptured
+
+    def getAllMoves(self):
+        moves = []        
+        for r in range(len(self.map)):
+            for c in range(len(self.map[r])):
+                unitIndex = self.map[r][c]
+                if unitIndex != -1:
+                    piece = self.unitList[unitIndex]
+                    turn = piece.team
+                    if (turn == "red" and not self.blueToMove) or (turn == "blue" and self.blueToMove):                                        
+                        moves = getValidMoves(r, c, piece._move_range)
+        return moves
+                        
+    def squareContainsUnit(self, row, col):
+        return self.map[row][col] != -1
+
+    def getValidMoves(self, row, col, move_range):
+        moves = []
+        for i in range(1, move_range + 1):
+            for j in range(1, move_range + 1):
+                rt = row + j
+                ct = col + (i - j)
+                if self.validatePair(rt, ct):
+                    moves.append((Move((row, col), (rt, ct), self.map)))
+                rt = row + (i - j)
+                ct = col + j
+                if self.validatePair(rt, ct):
+                    moves.append((Move((row, col), (rt, ct), self.map)))
+                rt = row - j
+                ct = col - (i - j)
+                if self.validatePair(rt, ct):
+                    moves.append((Move((row, col), (rt, ct), self.map)))
+                rt = row + (i - j)
+                ct = col - j
+                if self.validatePair(rt, ct):
+                    moves.append((Move((row, col), (rt, ct), self.map)))
+        return moves
+
+    def validatePair(self, r, c):
+        r_in = ( r >= 0 and r < len(self.map))
+        c_in = ( c >= 0 and c < len(self.map[0]))
+        if not r_in or not c_in:
+            return False
+        no_unit = (self.map[r][c] < 0)
+        return no_unit
+        
+                
+
+
+    # for each 1 to move range
+        # look at the pair (row + j, col + (move_range - j))
+            # make sure no part of that goes over max or min
+        # look at the pair (row - j, col + (move_range - j))
+            # 
+
+
+class Move():
+    def __init__(self, startSq, endSq, map):
+        self.startRow = startSq[0]
+        self.startCol = startSq[1]
+        self.endRow = endSq[0]
+        self.endCol = endSq[1]
+        self.pieceMoved = map[self.startRow][self.startCol]
+        self.pieceCaptured = map[self.endRow][self.endCol]
+        self.moveID = self.startRow * pow(11, 3) + self.startCol * pow(11, 2) + self.endRow * 11 + self.endCol * 1
+
+    def __eq__(self, other):
+        if isinstance(other, Move):
+            return self.moveID == other.moveID
+        return False
+    
+
+class ArmyUnit:
+    def __init__(self, **kwargs):
+        self.attack_range = kwargs["attack_range"]
+        self.move_range = kwargs["move_range"]
+        self._hit_points = kwargs["hit_points"]
+        self._max_hit_points = kwargs["max_hit_points"]
+        self._team = "blue"
+        self._unit_name = kwargs["unit_name"]
+
+    def unit_name(self):
+        return self._unit_name
+
+    def team(self):
+        return self._team # team should really be an enum
+
+class FootSoldier(ArmyUnit):
+    def __init__(self, team):
+        kwargs = {
+            "attack_range": 1,
+            "move_range": 1,
+            "hit_points": 1,
+            "max_hit_points": 1,
+            "unit_name": "footsoldier"
+        }
+        super().__init__(**kwargs)
+        self._team = team        
+
+class GameState(Enum):
+    AWAITING_UNIT_SELECTION
---
+++ source/MainScript.py
@@ -0,0 +1,137 @@
+import pygame as p
+import EngineScript
+
+SCALE = 4
+BOARD_X = 8
+BOARD_Y = 11
+SQ_SIZE = 16*SCALE
+BOARD_WIDTH = BOARD_X*SQ_SIZE
+BOARD_HEIGHT = BOARD_Y*SQ_SIZE
+WALLSIZE = 8*SCALE
+MENU_WIDTH = BOARD_WIDTH
+MENU_HEIGHT = 16*SCALE # one square for now
+WIDTH = 128*SCALE
+HEIGHT = 192*SCALE
+MAX_FPS = 15
+IMAGES = {}
+BOARDART = p.image.load("Sprites/field.png")
+BOARDART = p.transform.scale(BOARDART, (BOARD_WIDTH, BOARD_HEIGHT))
+
+
+def loadImages():
+    teams = ["b", "r"]
+    unit_types = ["footsoldier"]
+    for team in teams:
+        for unit_type in unit_types:
+            IMAGES[team, unit_type] = p.image.load("Sprites/" + team + "_" + unit_type + ".png")
+            IMAGES[team, unit_type] = p.transform.scale(IMAGES[team, unit_type], (SQ_SIZE, SQ_SIZE))
+
+
+def main():
+    p.init()
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    gs = EngineScript.GameState()
+    validMoves = gs.getAllMoves()
+    moveMade = False # flag variable for when a move is made
+    loadImages()
+    running = True
+    sqSelected = () #tracks the row and the column
+    playerClicks = [] #two tuples that keep track of the two squares the player has clicked
+    kwargs = {
+        "attack_range": 1,
+        "move_range": 1,
+        "hit_points": 1,
+        "max_hit_points": 1,
+        "unit_name": "Foot Soldier",
+        "team": "blue"
+    }
+    thisUnit = EngineScript.FootSoldier
+    #thisUnit = EngineScript.ArmyUnit(kwargs)
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+            elif e.type == p.MOUSEBUTTONDOWN:
+                target = p.mouse.get_pos()
+                if is_in_map(target):
+                    map_event_handler(target, gs.map)
+                elif is_in_menu(target):
+                    menu_event_handler(target, gs.map, gs.menu)
+            elif e.type == p.KEYDOWN:  # later move this to menu
+                if e.key == p.K_z:
+                    gs.undoMove()
+                    validMoves = gs.getAllMoves()        
+        draw_game_state(screen, gs, validMoves, sqSelected)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+def highlightSqures(screen, gs, validMoves, sqSelected):
+    if sqSelected != ():
+        r, c = sqSelected
+        s = p.Surface((SQ_SIZE - SCALE / 2, SQ_SIZE - SCALE / 2))
+        s.set_alpha(100) # 255 is opaque
+        s.fill(p.Color('blue'))
+        screen.blit(s, (c*(SQ_SIZE), r*(SQ_SIZE) + WALLSIZE)) # add y offset later
+        s.fill(p.Color('blue'))
+        for move in validMoves:
+            if move.startRow == r and move.startCol == c:
+                screen.blit(s, (move.endCol*SQ_SIZE, move.endRow*SQ_SIZE + WALLSIZE)) # add y offset later
+
+
+def draw_game_state(screen, gs, validMoves, sqSelected, gameState):
+    if gameState = 
+
+
+def drawBoard(screen):
+    screen.blit(BOARDART, p.Rect(0, 0, WIDTH, HEIGHT))
+
+
+def drawPieces(screen, map, unitList):
+    for r in range(BOARD_Y):
+        for c in range(BOARD_X):
+            # print(f"the vertical value is {r*SQ_SIZE+WALLSIZE}")
+            index = map[r][c]
+            if index != -1:
+                thisUnit = unitList[index]
+                thisType = thisUnit.unit_name()
+                thisTeam = thisUnit.team()
+                screen.blit(IMAGES[thisTeam, thisType], p.Rect(c*SQ_SIZE, r*SQ_SIZE+WALLSIZE, SQ_SIZE, SQ_SIZE))
+
+def animateMove(move, screen, clock, gs):
+    coords = []
+    dR = move.endRow - move.startRow
+    dC = move.endCol - move.startCol
+    framesPerSquare = 10
+    frameCount = (abs(dR) + abs(dC)) + framesPerSquare
+    for frame in range(frameCount + 1):
+        r, c = (move.startRow + dR*frame/frameCount, move.startCol + dC*frame/frameCount)
+        drawBoard(screen)
+        drawPieces(screen, gs.map, gs.unitList)
+        # erase the piece from its ending square
+        endSquare = p.Rect(move.endCol*SQ_SIZE, move.endRow*SQ_SIZE + WALLSIZE, SQ_SIZE, SQ_SIZE)
+        screen.blit(BOARDART, endSquare, endSquare)
+        # show moving piece
+        screen.blit(IMAGES[gs.unitList[move.pieceMoved].team(), gs.unitList[move.pieceMoved].unit_name()], p.Rect(c*SQ_SIZE, r*SQ_SIZE + WALLSIZE, SQ_SIZE, SQ_SIZE))
+        p.display.flip()
+        clock.tick(60)        
+
+def drawMenu(menu, screen):
+    menu_bg = p.Surface((MENU_WIDTH, MENU_HEIGHT))
+    menu_bg.fill(p.Color('black'))
+    screen.blit(0, BOARD_HEIGHT, menu_bg)
+    button_width = SCALE
+    starting_x = SCALE*2 # two scaled "pixels"
+    starting_y = BOARD_HEIGHT + SCALE*2
+    button_spacing_x = SQ_SIZE * 3
+    draw_x = SCALE*2
+    draw_y = starting_y
+    for button in menu.buttons:
+        button_s = p.Surface(())
+
+
+
+
+if __name__ == "__main__":
+    main()
