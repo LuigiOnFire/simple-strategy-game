66f767064b066d9c542039815fb76dd01b39ec06
William Crawford <crawfowa@gmail.com>
2023-11-28 19:02:00 -0800

    Geometry drawing for menu

--- source/menu_menu.py
+++ source/menu_menu.py
@@ -1,37 +1,44 @@
 import pygame as p
 
 class MenuMenu():
-    def __init__(self, x, y):
+    def __init__(self):
         self.buttons = []
         self.alignment = "l" # l - left, c - center, r - right
         self.width = 0
         self.height = 0
         self.border_width = 0
-        self.x = x
-        self.y = y
+        self.border_color = None
+        self.bg_color = None
 
 
     def gen_surface(self):
-        self.width = 0
-        self.height = 0
+        inner_width = 0
+        inner_height = 0
         for button in self.buttons:
             # find the dims of the buttons
             width, height = button.find_dims()
-            if width >= self.width:
-                self.width = width
+            if width >= inner_width:
+                inner_width = width
 
-            self.height += height
+            inner_height += height
 
-        self.width += 2 * self.border_width
-        self.height += 2 * self.border_width
+        self.width += 2 * inner_width
+        self.height += 2 * inner_width
 
         # generate border rect
-        outer_surface = p.Rect(self.x, self.y, self.width, self.height)
-        screen
+        menu_surface = p.Surface(self.width, self.height)
+
+        border_surface = p.Surface(self.width, self.height)
+        border_surface.fill(self.border_color)
+        menu_surface.blit(border_surface)
+
+        inner_surface = p.Surface(inner_width, inner_height)
+        inner_surface.fill(self.bg_color)
+        menu_surface.blit(inner_surface, (self.border_width, self.border_width))
 
         # generate button bg
 
-        # generate each button surface with the correct menu-wide dimensions            
+        # generate each button surface with the correct menu-wide dimensions
 
     def add_button(self, button):
         self.buttons.append(button)
